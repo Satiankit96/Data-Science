e5e84bc25e9c09cedf6a01ca1eb3b8fb5da8d533
Satiankit96 <ankit.sati05@gmail.com>
2023-11-19 06:57:27 -0500

    Numpy

---
+++ IBM/4. Python for Data Science, AI & Development/Week_4/Pandas.py
@@ -0,0 +1,89 @@
+#Unique
+# df['Released'].unique()
+#Pandas
+# https://author-ide.skills.network/render?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZF9pbnN0cnVjdGlvbnNfdXJsIjoiaHR0cHM6Ly9jZi1jb3Vyc2VzLWRhdGEuczMudXMuY2xvdWQtb2JqZWN0LXN0b3JhZ2UuYXBwZG9tYWluLmNsb3VkL0lCTURldmVsb3BlclNraWxsc05ldHdvcmstUFkwMTAxRU4tU2tpbGxzTmV0d29yay9sYWJzL2xhYi9tb2R1bGVfNC93cml0ZV9maWxlX3dpdGhfb3Blbl9yZWFkaW5nLm1kIiwidG9vbF90eXBlIjoiaW5zdHJ1Y3Rpb25hbC1sYWIiLCJhZG1pbiI6ZmFsc2UsImlhdCI6MTY5Njk3NDcxOH0.pAbY0tXv6dgSlWceY5r3HKXG_AMos8CWeYWfmCZ_CzA
+# iloc - index location
+# loc - value location
+
+# Why Numpy over lists ?
+# Faster than lists as it uses fixed data types.
+# Lists uses a lot more information as compared to numpy.(Check type of each value in list)
+# Numpy uses contiguous memory. Which means storage next to each other. (Lists uses a pointer to the memory)
+import numpy as np
+a = np.array([1,2,3], dtype = 'int32') # To be more efficient it is always a good idea to mention the dtypes 
+print(a)
+
+b = np.array([[1.0,2.0,3.0],[4.0,5.0,6.0]])
+print(b)
+
+# Printing the Dimensions 
+print(a.ndim)
+
+# Shape 
+print(a.shape)
+print(b.shape)
+# type
+print(a.dtype)
+
+# Size 
+print(a.itemsize)
+
+# Number of elements in the array
+print("Number of elemensts are", +a.size)
+
+# Total Size (This is equal to number of bytes as well)
+print (a.size * a.itemsize)
+
+a = np.array([[1,2,3,4,5,6,7],[8,9,10,11,12,13,14]])
+print(a.shape)
+
+# getting specific index [r,c]
+print(a[1,5])
+
+#Get a specific row 
+print(a[0,:])
+
+# Get a specific Column [3]
+print(a[:,2])
+
+# skipping the parts of the list [startindex:endindex:stepsize]
+print(a[0,1:-1:2])
+
+# changing the elements of the array 
+a[1,2] = 69
+print(a)
+
+a[:,3] = [1,1]
+print(a)
+
+
+# Moving to the 3d arrays 
+a = np.array([[[1,2],[3,4]],
+              [[5,6],[7,8]]])
+print(a)
+
+# Accessing the elemets of the array - Move outside in 
+print(a[1,0,0])
+
+# Replacing a row
+
+a[0,1,:] = [20,15]
+print(a)  
+
+# numbers 
+a = np.ones((4,2,2),dtype='int32') 
+print (a)
+
+a = np.zeros((4,2,2),dtype='int32') 
+print(a)
+
+# Any other number np.full 
+print(np.full((2,2),99))
+
+# Random
+print(np.random.random_sample(a.shape))
+
+print(np.random.rand(4,2))
+
+# ?Random integer value. max values up until 9. 
+print(np.random.randint(9, size=(3,3)))
